b770009dd32e2ee3f691685b5c5b50e3872be8fb
mhanuz <46758023+mhanuz@users.noreply.github.com>
2023-01-30 11:05:23 +0600

    Update optional Parameter.ts

--- src/optional Parameter.ts
+++ src/optional Parameter.ts
@@ -1,7 +1,7 @@
 // when it's not mandatory for that parameter to have a value
 function showDetails(p1: string, p2: number, p3?:string ): void {
     console.log(p1,p2);
-    if(p3 !==undefined){
+    if(p3 !==undefined){ // write only the parameter name don't check the condition with undefined, not good practice
         console.log(p3);
         
     }
